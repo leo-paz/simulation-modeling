c6d48b4fe7601edc3ade0d524c2357b3edcb86fe
Roustom <47872392+Roustom@users.noreply.github.com>
2021-03-11 17:51:27 -0500

    fixed component names and added Buffer class

---
+++ src/Buffer.java
@@ -0,0 +1,18 @@
+import java.util.concurrent.ArrayBlockingQueue;
+
+public class Buffer {
+    private ArrayBlockingQueue<Component> queueBuffer;
+    private final int MAX_CAPACITY = 3;
+
+    public Buffer(){
+        queueBuffer = new ArrayBlockingQueue<>(MAX_CAPACITY);
+    }
+
+    public ArrayBlockingQueue<Component> getBuffer(){
+        return this.queueBuffer;
+    }
+
+    public void addElement(Component c){
+
+    }
+}

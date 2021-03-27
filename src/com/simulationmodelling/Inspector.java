c546f861896d369f49c94b04dedc4ba929002713
Roustom <47872392+Roustom@users.noreply.github.com>
2021-03-27 16:29:42 -0400

    fixed inspector class

--- src/com/simulationmodelling/Inspector.java
+++ src/com/simulationmodelling/Inspector.java
@@ -7,16 +7,22 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Inspector {
-   private int inspectorId;
-   private float lambda;
-   private ArrayList<Float> procTimes;
-   private Random r;
-   private ArrayList<Buffer> buffers;
+    private int inspectorId;
+    private float lambda;
+    protected int procIndex;
+    private ArrayList<Float> procTimes;
+    public int iterations;
+    private Random r;
+    private ArrayList<Buffer> buffers;
 
-   public Inspector(String pathToData) throws FileNotFoundException{
-       r = new Random();
-       buffers = new ArrayList<>();
-       File file = new File(pathToData);
+    public Inspector(String pathToData, int iters, int id) throws FileNotFoundException {
+        r = new Random();
+        inspectorId = id;
+        iterations = iters;
+        procTimes = new ArrayList<>();
+        procIndex = 0;
+        buffers = new ArrayList<Buffer>();
+        File file = new File(pathToData);
         Scanner scanner = new Scanner(file);
         while (scanner.hasNextLine()) {
             try {
@@ -25,77 +31,84 @@ public class Inspector {
                 System.out.printf("Failed to read string, but continuing: %s\n", e.toString());
             }
         }
+        lambda = calculateMean();
 
-   }
+    }
 
-
-   private float calculateMean() {
+    private float calculateMean() {
         float sum = 0.0f;
-        for (Float num: procTimes) {
+        for (Float num : procTimes) {
             sum += num;
         }
         return sum / 300;
     }
-   public void addBuffer(Buffer buffer){
-       this.buffers.add(buffer);
-   }
 
+    public Float getNextServiceTime() {
+        var rand = new Random();
+        var num = rand.nextDouble();
+        return (float) (Math.log(1 - num) / (-lambda));
+    }
 
-   /**
-    * Puts a random component onto a buffer
-    */
-   public void putComponents(){
-       while(true){
-        Component temp = generateComponent(1234);
-        for(Buffer b : this.buffers){
-            if(b.getSize()==0){
-                b.addComponent(temp);
-                return;
+    public void addBuffer(Buffer buffer) {
+        this.buffers.add(buffer);
+    }
+
+    /**
+     * Puts a random component onto a buffer
+     */
+    public void putComponents() {
+        while (true) {
+            Component temp = generateComponent(1234);
+            for (Buffer b : this.buffers) {
+                if (b.getSize() == 0) {
+                    b.addComponent(temp);
+                    return;
+                }
+            }
+            for (Buffer b : this.buffers) {
+                if (b.getSize() == 1) {
+                    b.addComponent(temp);
+                    return;
+                }
+            }
+            try {
+                this.wait();
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
             }
         }
-        for(Buffer b : this.buffers){
-            if(b.getSize()==1){
-                b.addComponent(temp);
-                return;
-            }
-        }
-        try {
-            this.wait();
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-       }
-       
-        //IF IT GETS HERE THEN ITS BLOCKED FROM PUTTING COMPONENTS
-   }
 
-   /**
-    * Generates a random component
-    * @param seed
-    * @return
-    */
-   public Component generateComponent(long seed){
-       r.setSeed(seed);
-       int randomNumber = r.nextInt();
+        // IF IT GETS HERE THEN ITS BLOCKED FROM PUTTING COMPONENTS
+    }
+
+    /**
+     * Generates a random component
+     * 
+     * @param seed
+     * @return
+     */
+    public Component generateComponent(long seed) {
+        r.setSeed(seed);
+        int randomNumber = r.nextInt();
         randomNumber = randomNumber % 3;
-        switch(randomNumber){
-            case 0:
-                return Component.C1;
-            case 1:
-                return Component.C2;
-            case 2:
-                return Component.C3;
-            default:
-                return generateComponent(seed);
+        switch (randomNumber) {
+        case 0:
+            return Component.C1;
+        case 1:
+            return Component.C2;
+        case 2:
+            return Component.C3;
+        default:
+            return generateComponent(seed);
         }
-   }
+    }
 
-public int getInspectorId() {
-    return inspectorId;
-}
+    public int getInspectorId() {
+        return inspectorId;
+    }
 
-public void setInspectorId(int inspectorId) {
-    this.inspectorId = inspectorId;
-}
+    public void setInspectorId(int inspectorId) {
+        this.inspectorId = inspectorId;
+    }
 }
